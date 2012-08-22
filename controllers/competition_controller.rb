cacc3f206c90a26fe2796d57e50b0fc56ada90e5
jackiekircher <kircher.jackie@gmail.com>
2012-08-21 23:31:05 -0400

    allow admins to actually create competitions

--- controllers/competition_controller.rb
+++ controllers/competition_controller.rb
@@ -5,8 +5,18 @@ class OSGCCWeb
     haml :'competitions/index', :layout => :home_layout
   end
 
+  post "/competitions" do
+    return 404 unless (logged_in? && current_user.admin?)
+
+    name       = params[:comp_name]
+    start_date = DateTime.strptime("#{params[:start_date]}T#{params[:start_time]}","%Y-%m-%dT%I:%M%P")
+    end_date   = DateTime.strptime("#{params[:end_date]}T#{params[:end_time]}","%Y-%m-%dT%I:%M%P")
+    Competition.create(:name => name, :start_date => start_date, :end_date => end_date)
+  end
+
   get '/competitions/new' do
     return 404 unless (logged_in? && current_user.admin?)
     haml :'competitions/new', :layout => :home_layout
   end
+
 end
--- spec/controllers/competition_controller_spec.rb
+++ spec/controllers/competition_controller_spec.rb
@@ -6,24 +6,74 @@ describe 'OSCGG-Web competitions' do
     OSGCCWeb
   end
 
-  describe "/competitions" do
+  let(:user) do
+    User.new(:uid => 1)
+  end
+
+  let(:admin_user) do
+    User.create(:uid => 2).tap do |u|
+      u.admin = 1
+      u.save
+    end
+  end
+
+  describe "GET /competitions" do
     it "loads" do
       get "/competitions"
       last_response.status.should == 200
     end
   end
 
-  describe "/competitions/new" do
-    let(:user) do
-      User.new(:uid => 1)
+  describe "POST /competitions", :type => :request do
+    it "returns a 404 if there is no logged in user" do
+      post '/competitions'
+      last_response.status.should == 404
     end
 
-    let(:admin_user) do
-      User.create(:uid => 2).tap do |u|
-        u.admin = 1
-        u.save
+    it "returns a 404 if the current user is not an admin" do
+      get '/auth/github/callback', nil, {"omniauth.auth" => mock_auth(user.uid)}
+      post '/competitions'
+      last_response.status.should == 404
+    end
+
+    context "when logged in as an admin" do
+      let(:params) do
+        {
+          :comp_name  => "test competition",
+          :start_date => "2012-06-06",
+          :start_time => "6:00pm",
+          :end_date   => "2012-06-07",
+          :end_time   => "6:00pm"
+        }
+      end
+
+      before :each do
+        get '/auth/github/callback', nil, {"omniauth.auth" => mock_auth(admin_user.uid)}
+      end
+
+      it "creates a competition" do
+        Competition.should_receive(:create)
+        post '/competitions', params
+      end
+
+      it "sets the competition name" do
+        post '/competitions', params
+        Competition.last.name.should == params[:comp_name]
+      end
+
+      it "parses the start date" do
+        post '/competitions', params
+        Competition.last.start_date.should_not be_nil
+      end
+
+      it "parses the end date" do
+        post '/competitions', params
+        Competition.last.end_date.should_not be_nil
       end
     end
+  end
+
+  describe "GET /competitions/new" do
 
     it "returns a 404 if there is no logged in user" do
       get '/competitions/new'
@@ -42,4 +92,5 @@ describe 'OSCGG-Web competitions' do
       last_response.status.should == 200
     end
   end
+
 end
