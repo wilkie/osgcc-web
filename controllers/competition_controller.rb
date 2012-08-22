f102e19d0ff583a5c80da87fddbe5b0430f79428
jackiekircher <kircher.jackie@gmail.com>
2012-08-21 21:12:06 -0400

    simple authorization for creating competitions

--- controllers/competition_controller.rb
+++ controllers/competition_controller.rb
@@ -4,4 +4,9 @@ class OSGCCWeb
     @competitions = Competition.all
     haml :'competitions/index', :layout => :home_layout
   end
+
+  get '/competitions/new' do
+    return 404 unless (logged_in? && current_user.admin?)
+    haml :'competitions/new', :layout => :home_layout
+  end
 end
--- spec/controllers/competition_controller_spec.rb
+++ spec/controllers/competition_controller_spec.rb
@@ -9,7 +9,37 @@ describe 'OSCGG-Web competitions' do
   describe "/competitions" do
     it "loads" do
       get "/competitions"
-      last_response.should be_ok
+      last_response.status.should == 200
+    end
+  end
+
+  describe "/competitions/new" do
+    let(:user) do
+      User.new(:uid => 1)
+    end
+
+    let(:admin_user) do
+      User.create(:uid => 2).tap do |u|
+        u.admin = 1
+        u.save
+      end
+    end
+
+    it "returns a 404 if there is no logged in user" do
+      get '/competitions/new'
+      last_response.status.should == 404
+    end
+
+    it "returns a 404 if the current user is not an admin" do
+      get '/auth/github/callback', nil, {"omniauth.auth" => mock_auth(user.uid)}
+      get '/competitions/new'
+      last_response.status.should == 404
+    end
+
+    it "lets admins create a new competition" do
+      get '/auth/github/callback', nil, {"omniauth.auth" => mock_auth(admin_user.uid)}
+      get '/competitions/new'
+      last_response.status.should == 200
     end
   end
 end
