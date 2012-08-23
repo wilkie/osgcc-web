d96d2ee53a28aab34702009cb97deb9fdb69b4a1
jackiekircher <kircher.jackie@gmail.com>
2012-08-22 21:01:00 -0400

    flesh out competition views a bit
    add an individual competition page and fill out a basic index page, along
    with the corresponding routes. Once we create a competition we want to
    redirect to the competition that was just created. There's a lot more
    functionality to add to the competition show page but for now we're content
    with just displaying the information.

--- controllers/competition_controller.rb
+++ controllers/competition_controller.rb
@@ -11,12 +11,19 @@ class OSGCCWeb
     name       = params[:comp_name]
     start_date = DateTime.strptime("#{params[:start_date]}T#{params[:start_time]}","%Y-%m-%dT%I:%M%P")
     end_date   = DateTime.strptime("#{params[:end_date]}T#{params[:end_time]}","%Y-%m-%dT%I:%M%P")
-    Competition.create(:name => name, :start_date => start_date, :end_date => end_date)
+    c = Competition.create(:name => name, :start_date => start_date, :end_date => end_date)
+
+    redirect "/competitions/#{c._id.to_s}"
   end
 
   get '/competitions/new' do
     return 404 unless (logged_in? && current_user.admin?)
     haml :'competitions/new', :layout => :home_layout
+  end
+
+  get '/competitions/:id' do
+    @competition = Competition.find(params[:id])
+    haml :'competitions/show', :layout => :home_layout
   end
 
 end
--- models/competition.rb
+++ models/competition.rb
@@ -4,4 +4,12 @@ class Competition
   key :name,       String
   key :start_date, Time
   key :end_date,   Time
+
+  def formatted_start
+    start_date.strftime('%A %B %-d, %Y %l:%M %P')
+  end
+
+  def formatted_end
+    end_date.strftime('%A %B %-d, %Y %l:%M %P')
+  end
 end
