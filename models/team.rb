7308ba57e5c039ae6bbcd11dee573de23efb2a8e
jackiekircher <kircher.jackie@gmail.com>
2012-08-24 22:34:57 -0400

    require a team to have a competition

--- models/team.rb
+++ models/team.rb
@@ -1,8 +1,9 @@
 class Team
   include MongoMapper::Document
 
-  key :name,     String,  :required => true
-  key :user_ids, Array
+  key :name,           String,  :required => true
+  key :user_ids,       Array,   :default => []
+  key :competition_id, Integer, :required => true
 
   belongs_to :competition
   many       :users, :in => :user_ids, :class_name => "User"
